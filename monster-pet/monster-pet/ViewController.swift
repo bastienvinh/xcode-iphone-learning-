0db54d3b3b84cf5e8355e86c06297f208861a967
Bastien VINH <bus.bvinh@gmail.com>
2016-03-05 04:32:59 +0000

    Finnish the first version of the game [UPDATE] Add sound to the game [FIX] Fix the problem with my third skull image [ADD] Add new song on repositories

--- monster-pet/monster-pet/ViewController.swift
+++ monster-pet/monster-pet/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -25,6 +26,15 @@ class ViewController: UIViewController {
     
     var _penalties = 0
     var _timer : NSTimer!
+    var monsterHappy = false
+    var currentItem: UInt32 = 0
+    
+    var musicPlayer: AVAudioPlayer!
+    var sfxBite: AVAudioPlayer!
+    var sfxHeart: AVAudioPlayer!
+    var sfxDeath: AVAudioPlayer!
+    var sfxSkull: AVAudioPlayer!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,11 +44,41 @@ class ViewController: UIViewController {
         heartImgView.dropTarget = monsterImgView
         foodImgView.dropTarget = monsterImgView
         
-        skull1ImgView.alpha = DIM_ALPHA
-        skull2ImgView.alpha = DIM_ALPHA
-        skull3ImgView.alpha = DIM_ALPHA
+        
+        skull1ImgView?.alpha = DIM_ALPHA
+        skull2ImgView?.alpha = DIM_ALPHA
+        skull3ImgView?.alpha = DIM_ALPHA
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.itemDroppedOnCharacter(_:)), name: "onTargetDropped", object: nil)
+        
+        
+        // TODO : Improve that, it is not super
+        do {
+            let path = NSBundle.mainBundle().pathForResource("cave-music", ofType: "mp3")
+            let url = NSURL(fileURLWithPath: path!)
+            
+            try musicPlayer = AVAudioPlayer(contentsOfURL: url)
+            
+            try sfxBite = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bite", ofType: "wav")!))
+            
+            try sfxHeart = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("heart", ofType: "wav")!))
+            
+            try sfxDeath = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("death", ofType: "wav")!))
+            
+            
+            try sfxSkull = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("skull", ofType: "wav")!))
+            
+            musicPlayer.prepareToPlay()
+            musicPlayer.play()
+            
+            sfxSkull.prepareToPlay()
+            sfxHeart.prepareToPlay()
+            sfxDeath.prepareToPlay()
+            sfxBite.prepareToPlay()
+            
+        } catch let err as NSError  {
+            print(err.debugDescription)
+        }
         
         startTimer()
     }
@@ -55,24 +95,54 @@ class ViewController: UIViewController {
     func changeGameStates() {
         _penalties += 1
         
-        if _penalties == 1 {
-            skull1ImgView.alpha = OPAQUE
-            skull2ImgView.alpha = DIM_ALPHA
-        } else if _penalties == 2 {
-            skull2ImgView.alpha = OPAQUE
-            skull3ImgView.alpha = DIM_ALPHA
-        } else if _penalties == 3 {
-            skull3ImgView.alpha = OPAQUE
-        }
-        else {
-            skull1ImgView.alpha = DIM_ALPHA
-            skull2ImgView.alpha = DIM_ALPHA
-            skull3ImgView.alpha = DIM_ALPHA
+        if !monsterHappy {
+            
+            sfxSkull.play()
+            
+            if _penalties == 1 {
+                skull1ImgView.alpha = OPAQUE
+                skull2ImgView.alpha = DIM_ALPHA
+            } else if _penalties == 2 {
+                skull2ImgView?.alpha = OPAQUE
+                skull3ImgView?.alpha = DIM_ALPHA
+            } else if _penalties == 3 {
+                skull3ImgView?.alpha = OPAQUE
+            }
+            else {
+                skull1ImgView.alpha = DIM_ALPHA
+                skull2ImgView.alpha = DIM_ALPHA
+                skull3ImgView?.alpha = DIM_ALPHA
+            }
+            
+            if _penalties >= MAX_PENALTIES {
+                gameOver()
+            }
         }
         
-        if _penalties >= MAX_PENALTIES {
-            gameOver()
+        
+        // what food is Okay and must choose to feed him
+        let rand = arc4random_uniform(2) // 0 or 1
+        
+        if rand == 0 {
+            foodImgView.alpha  = DIM_ALPHA
+            foodImgView.userInteractionEnabled = false
+            
+            heartImgView.alpha  = OPAQUE
+            heartImgView.userInteractionEnabled = true
+            
         }
+        else if (rand == 1) {
+            
+            foodImgView.alpha  = OPAQUE
+            foodImgView.userInteractionEnabled = true
+            
+            heartImgView.alpha  = DIM_ALPHA
+            heartImgView.userInteractionEnabled = false
+            
+        }
+        
+        currentItem = rand
+        monsterHappy = false
     }
     
     
@@ -80,16 +150,24 @@ class ViewController: UIViewController {
         _timer.invalidate()
         
         monsterImgView.playDeathAnimation()
-    }
-    
-    
-    func onTargetDropped(notif: AnyObject) {
-        
-        print("I DROPPED SOMETHING")
+        sfxDeath.play()
     }
     
     func itemDroppedOnCharacter(notif: AnyObject) {
-        print("I DROPPED SOMETHING OOOKKKKKKK")
+        monsterHappy = true
+        startTimer()
+        
+        foodImgView.alpha = DIM_ALPHA
+        foodImgView.userInteractionEnabled = false
+        
+        heartImgView.alpha = DIM_ALPHA
+        heartImgView.userInteractionEnabled = false
+        
+        if currentItem == 0 {
+            sfxHeart.play()
+        } else if currentItem == 1 {
+            sfxBite.play()
+        }
     }
  
 }
