313d1796fc94d835ccc4d8af32c25ffcd932bb59
Bastien VINH <bus.bvinh@gmail.com>
2016-03-19 08:58:03 +0000

    Modify project WebRequest-Example and add three example url to use in the application [PROJECT] Webrequest-Example [DESCRIPTION] only load web page in a view [ADD] Add three new button (swift blog page, Steak blog page and Celibrity blog page) which load web page

--- Webrequest-Example/Webrequest-Example/ViewController.swift
+++ Webrequest-Example/Webrequest-Example/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var container: UIView!
+    
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Add out subview
+        webView = WKWebView()
+        container.addSubview(webView)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        // Create a frame for the view
+        let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+        webView.frame = frame
+        
+        loadRequest("https://www.weheartswift.com/getting-started-with-ios-programming")
+    }
+    
+     
+    func loadRequest(url: String) {
+        
+        
+        
+        
+        // Prepare HTML request
+        let UrlStringSwift = url
+        let nsUrl = NSURL(string: UrlStringSwift)
+        let request = NSURLRequest(URL: nsUrl!)
+        
+        webView.loadRequest(request)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loadSiwft(sender: UIButton) {
+        loadRequest("https://www.weheartswift.com/getting-started-with-ios-programming")
     }
-
-
+    
+    @IBAction func loadGrossip(sender: UIButton) {
+        loadRequest("http://www.tmz.com/2016/03/18/rian-johnson-star-wars-director-sued-agent")
+    }
+    
+    @IBAction func loadSteak(sender: UIButton) {
+        loadRequest("http://www.steak-enthusiast.com/2016/03/easter-lamb-menu")
+    }
 }
 
